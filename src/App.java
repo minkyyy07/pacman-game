e0123cf184af3de3e392df5e0f78ef64246723d8
apple <nikitosiksymonenko@gmail.com>
2025-10-26 16:35:13 +0100

    .

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int rowCount = 21;
+        int columnCount = 19;
+        int titleSize = 32;
+        int boardWidth = columnCount * titleSize;
+        int boardHeight = rowCount * titleSize;
     }
 }
